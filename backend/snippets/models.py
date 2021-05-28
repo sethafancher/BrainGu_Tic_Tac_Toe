4c66aebcdae5c2179abab6cd64ae4c181b878e1a
Seth Fancher <sfancher@Seths-MacBook-Pro.local>
2021-05-28 13:30:45 -0400

    Initial commit

---
+++ backend/snippets/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    winner = models.TextField()
+   
+    class Meta:
+        ordering = ['created']
---
+++ backend/snippets/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
+
+class SnippetView(APIView):
+    
+    serializer_class = SnippetSerializer
+  
+    def get(self, request):
+        detail = [ {"id": detail.id, "winner": detail.winner}
+        for detail in Snippet.objects.all()]
+        return Response(detail)
+  
+    def post(self, request):
+        serializer = SnippetSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return  Response(serializer.data)
